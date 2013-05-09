5c38c7140616b4d2936eb0174cac5362520c6c13
Ashok Janardhanan <eposts@gmail.com>
2013-05-09 12:18:09 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#
+
 
 #
 # Table name: users
@@ -17,6 +30,9 @@ describe User do
     subject {@user}
     it{should respond_to(:name)}
     it{should respond_to(:email)}
+    it{should respond_to(:password)}
+    it{should respond_to(:password_confirmation)}
+    it{should respond_to(:remember_token)}
 
     it{should be_valid}
 
